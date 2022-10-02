b90e1ddc822fa237bfcdfbfee01aa6ee673380e3
JJ1312 <yeiyeithirteen@gmail.com>
2022-10-02 17:51:51 -0300

    Se agregan estilos y paginas(curso, perfil, registro y *editarcurso), ademas agregan validador de registro*[*=no terminado]

---
+++ js/registrovalidation.js
@@ -0,0 +1,37 @@
+$(function() {
+    $("#registration-form").validate({
+  
+    rules: {
+        firstname: {
+            required: true,
+        },
+        lastname: {
+            required: true
+        },
+        email:{
+          required: true,
+          email: true
+        },
+        password:{
+          required: true,
+          minlength: 5
+        }
+      },
+
+    messages: {
+        firstname: "Por favor, introduzca su nombre",
+        lastname: "Por favor, introduzca su apellido",
+        password: {
+            required: "Por favor proporcione una contraseña",
+            minlength: "Su contraseña debe tener al menos 5 caracteres."
+        },
+        email: "Por favor, introduce una dirección de correo electrónico válida"
+      },
+  
+      submitHandler: function(form) {
+        form.submit();
+      }
+  
+    });
+  
+  });
